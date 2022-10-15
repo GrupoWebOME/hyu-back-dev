ecd2e968d8030c73088ebd034e55a0f7fc3935a7
Sebastián Schiavulli <100809593+sebastian-schiavulli@users.noreply.github.com>
2022-10-14 23:24:45 -0300

    Create audit_installation_model.js

---
+++ models/audit_installation_model.js
@@ -0,0 +1,55 @@
+const {Schema, model} = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const auditInstallationSchema = new Schema({
+       installation_id: {
+              type: Schema.Types.ObjectId, 
+              ref: 'Installation', 
+              required: true
+       },
+       dealership_id: {
+              type: Schema.Types.ObjectId, 
+              ref: 'Dealership', 
+              required: true
+       },
+       audit_id: {
+              type: Schema.Types.ObjectId, 
+              ref: 'Audit', 
+              required: true
+       },
+       auditor_id: [{
+              type: Schema.Types.ObjectId, 
+              ref: 'Admin', 
+              required: true
+       }],
+       comment: {
+              type: String, 
+              required: false
+       },
+       photo: {
+              type: String, 
+              required: false
+       },
+       audit_status: {
+              type: String, 
+              trim: true
+       },
+       audit_date: {
+              type: Date,
+              default: Date.now
+       },
+       createdAt: {
+              type: Date,
+              default: Date.now
+       },
+       updatedAt: {
+              type: Date,
+              default: Date.now
+       }
+})
+
+auditInstallationSchema.plugin(uniqueValidator)
+
+const AuditInstallation = model('AuditInstallation', auditInstallationSchema)
+
+module.exports = AuditInstallation
