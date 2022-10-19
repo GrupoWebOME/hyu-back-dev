773729a6b0ae9fa8d80479efca0618446206f4d8
Sebastián Schiavulli <100809593+sebastian-schiavulli@users.noreply.github.com>
2022-10-18 21:12:27 -0300

    Create authenticationAdmin.js

---
+++ middlewares/authenticationAdmin.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken')
+
+const validate = async(request, response, next) => {
+
+    const {authorization} = request.headers
+
+    if(!authorization)
+        return response.status(401).json({code: 401,
+                                          msg: 'invalid credentials',
+                                          detail: 'you do not have permissions'})
+
+    let decodedToken = null
+
+    if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+        token = authorization.substring(7)
+    }
+
+    await jwt.verify(token, process.env.SECRET, function(err, decoded) {
+        if(err){
+            decodedToken = false
+        }
+        else{
+            decodedToken = decoded
+        }
+    })
+    console.log('entra', decodedToken.admin)
+
+    if(decodedToken === false)
+        return response.status(401).json({code: 401,
+                                          msg: 'invalid credentials',
+                                          detail: 'you do not have permissions'})
+    
+    request.jwt = decodedToken
+
+    next()
+}
+
+module.exports = {validate}
