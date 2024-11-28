8b6d86c9d4d7c7396ca1ed0135cb92604967a5a5
Sebastian Schiavulli <sbsgnr2@gmail.com>
2024-11-28 09:38:26 -0300

    feat: update reviewMailContent

--- controllers/auditInstallation/auditInstallation_controller.js
+++ controllers/auditInstallation/auditInstallation_controller.js
@@ -33,18 +33,18 @@ const reviewMailContent = (installation, audit) => { return `
       auditor y se encuentra en Pendiente revisar concesión.
     </p> 
     <p>
-      Antes de publicar los resultados definitivos, queremos ofrecerte la posibilidad de revisar aquellos incumplimientos que consideres puedan ser susceptibles de 
-      emitir alegaciones para ser revisadas por HMES. En caso de que compruebes, que efectivamente, existe una justificación razonable para alegar un incumplimiento, 
+      Antes de publicar los resultados definitivos, queremos ofrecerte la posibilidad de revisar aquellos incumplimientos que consideres puedan ser susceptibles 
+      de emitir alegaciones para ser revisadas por HMES. En caso de que compruebes, que efectivamente, existe una justificación razonable para alegar un incumplimiento, 
       podrás realizar la alegación incluyendo toda aquella información que consideréis necesaria, fotos y/o comentarios.
     </p>
     <p>
       <b>
-        El plazo de alegaciones será de una semana contado a partir del día de hoy.
+        El plazo de alegaciones será hasta el día 11/12/2024 inclusive.
       </b>
     </p>
     <p>
-      Esta información se encuentra accesible a través de la aplicación Hyundai Standards Application (HSA) en el apartado Auditorías/ Alegaciones. 
-      Para cualquier duda contactar con Elena Drandar: estandares-hyundai@redhyundai.com
+      Esta información se encuentra accesible a través de la aplicación Hyundai Standards Application (HSA) en el apartado Auditorías/ Alegaciones. Para cualquier duda contactar con Elena Drandar: 
+      estandares-hyundai@redhyundai.com
     </p>
     <p>
       Recibe un cordial saludo,
